f152e120e80070c46ed30d6d7e8b9a03fe383e5e
Alec Milton <speedviper98@Alecs-MacBook-Pro-3.local>
2016-04-20 17:12:48 -0400

    twitterbot

--- tweetbot.py
+++ tweetbot.py
@@ -1,14 +1,19 @@
 from twython import Twython, TwythonError
+import json
+import requests
 
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN = ''
-ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = '3atqlB1OilzcWPga7f6EIv3Cf'
+CONSUMER_SECRET = 'HiHfmC1Rs4pL96kXto4lGcwPXDDjl86olRC3zP7AhBBqP2efAD'
+ACCESS_TOKEN = '722879150684381186-EQBYL8wY2yjpSavegm3CYIsNvBcP0C5'
+ACCESS_TOKEN_SECRET = 'rtt4GSZGCjBWPqNzDcmnanBbUpz8s6XCHjFwVdi0JmGSq'
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
+r= request.get()
+json = r.json
+
 try:
-    twitter.update_status(status='See how easy this was?')
+    twitter.update_status(status='Wow! that was easy!')
 except TwythonError as e:
     print e
 
